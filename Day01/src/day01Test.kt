548a8a8068033d7e649d0fc70056773ad4fe8be1
Herr Senf <me@your.mom>
2025-12-12 15:42:30 +0100

    Add day 1 puzzle

---
+++ Day01/src/day01Test.kt
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day01Test {
+
+  @Test
+  fun `start at 11 add 8 to right results in 19`() {
+    val result = solveToPassword(arrayOf("R8"), 11, 100)
+
+    assertEquals(19, result.first)
+  }
+
+  @Test
+  fun `start at 5 add 10 to left results in 95`() {
+    val result = solveToPassword(arrayOf("L10"), 5, 100)
+
+    assertEquals(95, result.first)
+  }
+
+  @Test
+  fun solveToDemo() {
+    val sequence: Array<String> =
+      arrayOf(
+        "L68",
+        "L30",
+        "R48",
+        "L5",
+        "R60",
+        "L55",
+        "L1",
+        "L99",
+        "R14",
+        "L82",
+      )
+
+    val result = solveToPassword(sequence, 50, 100)
+
+    assertEquals(32, result.first)
+    assertEquals(3, result.second)
+  }
+}
